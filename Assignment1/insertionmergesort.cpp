c00d17acc348c7cc35fc48c88734657ecc4a2b55
Jesse Holland <holland.jesse55@gmail.com>
2020-09-10 10:32:27 -0600

    Cleaned up code

--- Assignment1/insertionmergesort.cpp
+++ Assignment1/insertionmergesort.cpp
@@ -9,77 +9,112 @@ using namespace std::chrono; //code for chrono taken from https://www.geeksforge
 //Insertion Sort:
 //A=array; n is size of array
 template <class T>
-void insertSort(IntIndColl<T> &A, int n){
-    for (int i=1;i<=n-1;i++){           //iterates through each element in array
+void insertSort(IntIndColl<T> &A, int n)
+{
+    //iterates through each element in array
+    for (int i = 1; i <= n - 1; i++)
+    {
         T v = A[i];
         T j = i - 1;
-        while (j>=0 && A[j]>v){         //checks for correct position and places it there
-            A[j+1] = A[j];
+        //checks for correct position and places it there
+        while (j >= 0 && A[j] > v)
+        {
+            A[j + 1] = A[j];
             j--;
         }
-        A[j+1] = v;
+        A[j + 1] = v;
     }
 }
 
 //merge arrays B and C into A; n is size of A
 template <class T>
-void merge(IntIndColl<T> &B, IntIndColl<T> &C, IntIndColl<T> &A, int n){
-    T i = 0, j = 0, k = 0;            //iterators
-    T p = (n/2);                      //size of B
-    T q = n-(n/2);                    //size of C
-    while (i < p && j < q){             //checks for smallest num until either iterator is equal to the arrays size
-        if (B[i] <= C[j]){
+void merge(IntIndColl<T> &B, IntIndColl<T> &C,
+           IntIndColl<T> &A, int n)
+{
+    T i = 0, j = 0, k = 0; //iterators
+    T p = (n / 2);         //size of B
+    T q = n - (n / 2);     //size of C
+    //loops until one iterator is equal to the array size
+    while (i < p && j < q)
+    {
+        if (B[i] <= C[j])
+        {
             A[k] = B[i];
             i++;
         }
-        else{
+        else
+        {
             A[k] = C[j];
             j++;
         }
         k++;
     }
-    if (i == p){
-        for (int x=k;x<(p+q);x++)       //copy A[k..p+q-1] to C[j..q-1]
-            A[x] = C[j+x-k];            //copies from j+iterator minus the offset of k
+    if (i == p)
+    {
+        //copy A[k..p+q-1] to C[j..q-1]
+        for (int x = k; x < (p + q); x++)
+            //copies from j+iterator minus the offset of k
+            A[x] = C[j + x - k];
     }
-    else{
-        for (int x=k;x<(p+q);x++)       //copy A[k..p+q-1] to B[i..p-1]
-            A[x] = B[i+x-k];            //copies from i+iterator minus the offset of k
+    else
+    {
+        //copy A[k..p+q-1] to B[i..p-1]
+        for (int x = k; x < (p + q); x++)
+            //copies from i+iterator minus the offset of k
+            A[x] = B[i + x - k];
     }
 }
 
 //Merge Sort:
 //A=array; n is size of array
 template <class T>
-void mergeSort(IntIndColl<T> &A, int n){
-    if(n>1){
-        IntIndColl<T> B(n/2);         //create array for first half if A
-        IntIndColl<T> C(n-(n/2));     //create array for second half if A
-        for (int i=0;i<(n/2);i++)       //copy first half of A into B
+void mergeSort(IntIndColl<T> &A, int n)
+{
+    if (n > 1)
+    {
+        //create array for first half if A
+        IntIndColl<T> B(n / 2);
+        //create array for second half if A
+        IntIndColl<T> C(n - (n / 2));
+        //copy first half of A into B
+        for (int i = 0; i < (n / 2); i++)
             B[i] = A[i];
-        for (int i=0;i<n-(n/2);i++)     //copy second half of A into C
-            C[i] = A[(n/2)+i];
-        mergeSort(B, (n/2));            //recursive call to further split up arrays
-        mergeSort(C, (n-(n/2)));
-        merge(B, C, A, n);              //merge arrays together after they have been separated out
+        //copy second half of A into C
+        for (int i = 0; i < n - (n / 2); i++)
+            C[i] = A[(n / 2) + i];
+        //recursive call to further split up arrays
+        mergeSort(B, (n / 2));
+        mergeSort(C, (n - (n / 2)));
+        merge(B, C, A, n); //merge arrays together
     }
+}
+
+int oddeven()
+{
+    if (rand() % 2 == 0)
+        return 1;
+    else
+        return -1;
 }
 
 int main(int argc, char **argv)
 {
-    if (argc < 2){
+    if (argc < 2)
+    {
         cerr << "Usage: ./imsort <size of array> <upper bound for int> <upper bound for long long int>" << endl;
         exit(0);
     }
     int size, ubound;
     long long int lliubound;
-    srand(700445452);                           //set seed for rng
-    size = atoi(argv[1]);                       //set size of array
-    if (argc > 2){                              //set upper bound
+    srand(700445452);     //set seed for rng
+    size = atoi(argv[1]); //set size of array
+    if (argc > 2)
+    { //set upper bound
         ubound = atoi(argv[2]);
         lliubound = strtoll((argv[3]), nullptr, 0);
     }
-    else{
+    else
+    {
         ubound = 2147483647;
         lliubound = 9223372036854775807;
     }
@@ -87,50 +122,53 @@ int main(int argc, char **argv)
     IntIndColl<int> arr(size);
     IntIndColl<long long int> lliarr(size);
 
-    //need to make dynamic and remove size from above
-    for (int i = 0; i < size; i++)              //input random number based on seed into the array
+    for (int i = 0; i < size; i++) //input random number based on seed into the array
     {
-        arr[i] = rand() % ubound;//add multiplcation by INT_MAX and RAND_MAX
-        lliarr[i] = (rand()*4294967298) % lliubound;   //set upper limit of rand to long long int
+        arr[i] = (rand() * oddeven()) % ubound;
+        lliarr[i] = (rand() * 4294967298 * oddeven()) % lliubound; //set upper limit of rand to long long int
         /*cout << "A[" << i << "] = " << arr[i] << endl;        //output numbers contained in array
         cout << "Along[" << i << "] = " << lliarr[i] << endl;*/
     }
 
-    IntIndColl<int> arr2(size);       //create new array for second sort
+    IntIndColl<int> arr2(size); //create new array for second sort
     IntIndColl<long long int> lliarr2(size);
-    arr2.Copy(arr);                             //copy into new array
+    arr2.Copy(arr); //copy into new array
     lliarr2.Copy(lliarr);
 
     cout << "Starting Insertion Sort" << endl;
     auto start = high_resolution_clock::now();
-    insertSort(arr, size);                   //run insertion sort
+    insertSort(arr, size); //run insertion sort
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
-    cout << "Insertion Sort Time: " << duration.count() << " microseconds" << endl << endl;
+    cout << "Insertion Sort Time: " << duration.count() << " microseconds" << endl
+         << endl;
 
     cout << "Starting Merge Sort" << endl;
     start = high_resolution_clock::now();
-    mergeSort(arr2, size);                   //run merge sort
+    mergeSort(arr2, size); //run merge sort
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
-    cout << "Merge Sort Time: " << duration.count() << " microseconds" << endl << endl;
+    cout << "Merge Sort Time: " << duration.count() << " microseconds" << endl
+         << endl;
 
     cout << "Starting Insertion Sort with long long ints" << endl;
     start = high_resolution_clock::now();
-    insertSort(lliarr, size);                //run insertion sort
+    insertSort(lliarr, size); //run insertion sort
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
-    cout << "Long Long Int Insertion Sort Time: " << duration.count() << " microseconds" << endl << endl;
+    cout << "Long Long Int Insertion Sort Time: " << duration.count() << " microseconds" << endl
+         << endl;
 
     cout << "Starting Merge Sort with long long ints" << endl;
     start = high_resolution_clock::now();
-    mergeSort(lliarr2, size);                //run merge sort
+    mergeSort(lliarr2, size); //run merge sort
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop - start);
-    cout << "Long Long Int Merge Sort Time: " << duration.count() << " microseconds" << endl << endl;
+    cout << "Long Long Int Merge Sort Time: " << duration.count() << " microseconds" << endl
+         << endl;
 
     //[DEBUGGING]
-    cout << "Insertion Sort:" << endl;
+    /*cout << "Insertion Sort:" << endl;
     for (int i = 0; i < size; i++)            //output numbers to prove it is sorted
     {
         cout << arr[i] << endl;
@@ -149,6 +187,6 @@ int main(int argc, char **argv)
     for (int i = 0; i < size; i++)              //output numbers to prove it is sorted
     {
         cout << lliarr2[i] << endl;
-    }
+    }*/
     return 0;
 }
--- Assignment1/linearSort.cpp
+++ Assignment1/linearSort.cpp
@@ -3,19 +3,39 @@
 #include "../array/IntIndColl.cpp"
 
 using namespace std;
+
 //Linear search:
-//A=array; size is size of array; k is number being searched for
+//A=array; size = size of array; k = number being searched for
 bool linSort(IntIndColl<int> A, int size, int k)
 {
     int i = 0;
     bool found = false;
-    while ((size-i-i)>0 && !found)
-    { //loop until forward and backward search meet or number is found
-        if (A.Retrieve(i) == k)                 //searches forward
+    while (i < size && !found)
+    { //loop until end of array or number is found
+        if (A.Retrieve(i) == k)
             found = true;
-        if (A.Retrieve(size-(i+1)) == k)        //searches backwards
+        cout << "i: " << i << "  found: "
+             << boolalpha << found << endl;
+        i++;
+    }
+    return found;
+}
+
+//Two way Linear search:
+//A=array; size is size of array; k is number being searched for
+bool linSort2(IntIndColl<int> A, int size, int k)
+{
+    int i = 0;
+    bool found = false;
+    //loop until forward and backward search meet or k is found
+    while ((size - i - i) > 0 && !found)
+    {
+        if (A.Retrieve(i) == k) //searches forward
             found = true;
-        cout << "i: " << i << "  found: " << boolalpha << found << endl;
+        if (A.Retrieve(size - (i + 1)) == k) //searches backwards
+            found = true;
+        cout << "i: " << i << "  found: "
+             << boolalpha << found << endl;
         i++;
     }
     return found;
@@ -24,19 +44,24 @@ bool linSort(IntIndColl<int> A, int size, int k)
 int main()
 {
     int size, ubound;
-    srand(700445452);                           //set seed for rng
+    srand(700445452); //set seed for rng
     cout << "Size of array needed? ";
-    cin >> size;                                //get size of array
+    cin >> size; //get size of array
     cout << "Upper bound? ";
-    cin >> ubound;                              //set upper bound
+    cin >> ubound; //set upper bound
     IntIndColl<int> arr(size);
-    for (int i = 0; i < size; i++)              //input random number based on seed into the array
+
+    //input random number based on seed into the array
+    for (int i = 0; i < size; i++)
     {
         arr.Store(i, rand() % ubound);
-        cout << arr.Retrieve(i) << endl;        //output numbers contained in array
+        cout << arr.Retrieve(i) << endl;
     }
-    
-    //output the result of the search; currently just searches for 1
+    IntIndColl<int> arr2(size);
+    arr2.Copy(arr); //copy into new array
+
+    //output result of search; currently just searches for 1
     cout << boolalpha << linSort(arr, size, 1) << endl;
+    cout << boolalpha << linSort2(arr2, size, 1) << endl;
     return 0;
 }
